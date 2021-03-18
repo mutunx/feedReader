074b2b95cd48388fa63c630d7b9f4837b6d4e62e
mutun <tun224813@gmail.com>
2021-03-18 20:21:34 +0800

    实现解析xml

--- matchers/rss.go
+++ matchers/rss.go
@@ -1,14 +1,79 @@
 package matchers
 
 import (
+	"encoding/xml"
 	"feedReader/search"
-	"fmt"
+	"log"
+	"os"
+)
+
+// 定义rss结构体
+type (
+	// item defines the fields associated with the item tag
+	// in the rss document.
+	item struct {
+		XMLName     xml.Name `xml:"item"`
+		PubDate     string   `xml:"pubDate"`
+		Title       string   `xml:"title"`
+		Description string   `xml:"description"`
+		Link        string   `xml:"link"`
+		GUID        string   `xml:"guid"`
+		GeoRssPoint string   `xml:"georss:point"`
+	}
+
+	// image defines the fields associated with the image tag
+	// in the rss document.
+	image struct {
+		XMLName xml.Name `xml:"image"`
+		URL     string   `xml:"url"`
+		Title   string   `xml:"title"`
+		Link    string   `xml:"link"`
+	}
+
+	// channel defines the fields associated with the channel tag
+	// in the rss document.
+	channel struct {
+		XMLName        xml.Name `xml:"channel"`
+		Title          string   `xml:"title"`
+		Description    string   `xml:"description"`
+		Link           string   `xml:"link"`
+		PubDate        string   `xml:"pubDate"`
+		LastBuildDate  string   `xml:"lastBuildDate"`
+		TTL            string   `xml:"ttl"`
+		Language       string   `xml:"language"`
+		ManagingEditor string   `xml:"managingEditor"`
+		WebMaster      string   `xml:"webMaster"`
+		Image          image    `xml:"image"`
+		Item           []item   `xml:"item"`
+	}
+
+	// rssDocument defines the fields associated with the rss document.
+	rssDocument struct {
+		XMLName xml.Name `xml:"rss"`
+		Channel channel  `xml:"channel"`
+	}
 )
 
 type rssMatcher struct{}
 
 func (r rssMatcher) Search() {
-	fmt.Print("this is rss matcher\n")
+	// 获取文件
+	file, err := os.Open("data/example.xml")
+	if err != nil {
+		log.Fatalln("file not find")
+	}
+
+	defer file.Close()
+
+	// 解析到结构体中
+	var rss rssDocument
+	xml.NewDecoder(file).Decode(&rss)
+
+	log.Println("title", rss.Channel.Title)
+	log.Println("Description", rss.Channel.Description)
+	log.Println("Title", rss.Channel.Item[0].Title)
+	log.Println("Description", rss.Channel.Item[0].Description)
+
 }
 
 func init() {
